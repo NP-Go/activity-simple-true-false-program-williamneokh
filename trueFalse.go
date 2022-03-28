77813422220c936dbd6f74bdd5026889a2fafa25
williamneo <williamneokh@gmail.com>
2022-03-28 17:26:39 +0800

    Completed If statement

--- trueFalse.go
+++ trueFalse.go
@@ -4,18 +4,29 @@ import "fmt"
 
 func compare(value int) string {
 	//do not change this variable resultMessage, secretValue
-	resultMessge := ""
+	resultMessage := ""
 	secretValue := 88
+
+	if value == secretValue {
+		fmt.Println("Well done! your guess is correct.")
+	}
+	if value < secretValue {
+		fmt.Println("Too low, try again next time!")
+	}
+
+	if value > secretValue {
+		fmt.Println("Too high, try again next time!")
+	}
 
 	//Insert your code from here
 
 	//do not remove this line
-	return resultMessge
+	return resultMessage
 }
 
 func main() {
 	var guess int
-	fmt.Println("Enter an integer value: ")
+	fmt.Println("Enter an integer value from 1 to 100: ")
 	fmt.Scanln(&guess)
 	compare(guess)
 }
